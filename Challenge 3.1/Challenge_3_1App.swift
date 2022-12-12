499216d9b75ff1c767f20a1f6f67ba24a3cc48a9
Mark Holyfield <markholyfield23@Icloud.com>
2022-12-12 10:29:50 -0500

    Initial Commit

---
+++ Challenge 3.1/Challenge_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3_1App.swift
+//  Challenge 3.1
+//
+//  Created by Mark H on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 3.1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Challenge 3.1
+//
+//  Created by Mark H on 12/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
